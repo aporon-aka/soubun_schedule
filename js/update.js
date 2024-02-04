54fe63120ae3dad5bcf74cf4048efebc754a20be
aporon <148856972+aporon-aka@users.noreply.github.com>
2024-02-04 22:03:56 +0900

    Create update.js

---
+++ js/update.js
@@ -0,0 +1,8 @@
+/*更新日の取得*/
+const last = new Date(document.lastModified);
+const year = last.getFullYear();
+const month = last.getMonth()+1;
+const date = last.getDate();
+/*日付を書き換える*/
+const target = document.getElementById('modify');
+target.textContent = year + '-' + month + '-' + date;
